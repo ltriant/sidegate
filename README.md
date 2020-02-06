9d284393b381c59072600c356f977031f1b6ef2c
Luke Triantafyllidis <ltriant@cpan.org>
2020-02-06 11:31:58 +1100

    sidegate: when the front door is locked with proprietary crap

---
+++ README.md
@@ -0,0 +1,29 @@
+# SideGate
+
+Accept any and all file uploads
+
+# Why?
+
+On many occasions I've had friends who have wanted to send large files to me, but have run into the following issues:
+
+1. My MacBook only has USB-C ports, so I can't accept the files via a flash drive, or external hard drive, unless they magically have a USB-C cable, which they never do
+2. The friend's laptop isn't a MacBook, so we can't use AirDrop
+3. The files can be uploaded to a cloud file hosting provider, and then downloaded, but this seems wasteful of bandwidth when we're on the same local network
+4. The friend's laptop can't run a dead-simple fileserver (like Python's SimpleHTTPServer), because they don't have enough privileges
+
+All of this is a ridiculous and unfortunate reality, so I wrote this dirt-simple program that I can run on my laptop, and then any of my friends with a web browser can just dump files onto my laptop without needing to work around some kind of proprietary tech.
+
+# Build and Run
+
+The [Go runtime](https://golang.org/) is necessary to build. There are no dependencies, as the standard library was more than enough for this simple project.
+
+    $ go build
+    $ ./sidegate
+
+Get your local IP address (from `ifconfig` or whatever means you prefer), have the uploader browse to `http://1.2.3.4:8000` (where `1.2.3.4` is your IP address), and upload away.
+
+By default, files are dropped into the current working directory (i.e. whichever directory you ran the server from), but this can be overridden with the `DEST_DIR` environment variable.
+
+Also by default, the server listens on port 8000, but can be overridden by the `LISTEN_PORT` environment variable.
+
+    $ LISTEN_PORT=1234 DEST_DIR=/tmp ./sidegate
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"log"
+	"math"
+	"net/http"
+	"os"
+	"strings"
+)
+
+// These are embedded because otherwise the binary needs to find template HTML
+// files at runtime, which I don't wanna mess around with for now. And other
+// solutions require third-party dependencies, which I'm trying to avoid unless
+// absolutely necessary.
+const TEMPLATE_INDEX string = `<!DOCTYPE html><html><head><title>SideGate</title></head><body><form action="/upload" method="POST" enctype="multipart/form-data"><div><input type="file" name="file"></div><div><input type="submit" value="Upload"></div></form></body></html>`
+const TEMPLATE_UPLOAD string = `<!DOCTYPE html><html><head><title>SideGate</title></head><body><p>Upload successful!</p></body></html>`
+
+const DEFAULT_LISTEN_ADDRESS string = ":8000"
+
+func uploadHandler(w http.ResponseWriter, r *http.Request, destinationDir string) {
+	r.ParseMultipartForm(math.MaxInt32)
+
+	fin, header, err := r.FormFile("file")
+	if err != nil {
+		log.Fatalf("Unable to get `file` parameter: %v", err)
+	}
+	defer fin.Close()
+
+	name := header.Filename
+
+	// Create upload destination
+	var outputFile strings.Builder
+	outputFile.WriteString(destinationDir)
+	outputFile.WriteString("/")
+	outputFile.WriteString(name)
+
+	filePath := outputFile.String()
+
+	fout, err := os.Create(filePath)
+	if err != nil {
+		log.Fatalf("Unable to create file %s: %v", filePath, err)
+	}
+	defer fout.Close()
+
+	// Stream file to disk
+	bytes, err := io.Copy(fout, fin)
+	if err != nil {
+		log.Fatalf("Failed to save file to path %s: %v", filePath, err)
+	}
+
+	log.Printf("File uploaded to: %s (%d bytes)", filePath, bytes)
+
+	t, _ := template.New("upload-page").Parse(TEMPLATE_UPLOAD)
+	t.Execute(w, nil)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.New("index-page").Parse(TEMPLATE_INDEX)
+	t.Execute(w, nil)
+}
+
+func main() {
+	destinationDir, err := os.Getwd()
+
+	if err != nil {
+		log.Fatalf("Unable to get current working directory: %v", err)
+	}
+
+	if destDir, notEmpty := os.LookupEnv("DEST_DIR"); notEmpty {
+		destinationDir = destDir
+	}
+
+	listenAddress := DEFAULT_LISTEN_ADDRESS
+	if listenPort, notEmpty := os.LookupEnv("LISTEN_PORT"); notEmpty {
+		var listenAddrStr strings.Builder
+		listenAddrStr.WriteString(":")
+		listenAddrStr.WriteString(listenPort)
+		listenAddress = listenAddrStr.String()
+	}
+
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+		uploadHandler(w, r, destinationDir)
+	})
+
+	log.Printf("Saving uploads to %s", destinationDir)
+	log.Printf("Listening on %s", listenAddress)
+
+	log.Fatal(http.ListenAndServe(listenAddress, nil))
+}
