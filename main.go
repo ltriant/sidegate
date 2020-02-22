c4a450db8cac7bb6823c47d754ee13c8f84ac0d9
Luke Triantafyllidis <ltriant@cpan.org>
2020-02-22 23:10:09 +1100

    main: show directory contents
    It's nice to have more visual confirmation that an upload was
    successful, so I've added the directory contents of the directory that
    uploads are being streamed to on the index page. It also shows the file
    sizes, since that also helps with confirmation.
    
    The next step will be to optionally allow downloading of files from the
    directory, if this wants to be used in the opposite direction, like
    Python's SimpleHTTPServer.

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"html/template"
 	"io"
+	"io/ioutil"
 	"log"
 	"math"
 	"net/http"
@@ -16,8 +18,82 @@ import (
 // files at runtime, which I don't wanna mess around with for now. And other
 // solutions require third-party dependencies, which I'm trying to avoid unless
 // absolutely necessary.
-const TEMPLATE_INDEX string = `<!DOCTYPE html><html><head><title>SideGate</title></head><body><form action="/upload" method="POST" enctype="multipart/form-data"><div><input type="file" name="file"></div><div><input type="submit" value="Upload"></div></form></body></html>`
-const TEMPLATE_UPLOAD string = `<!DOCTYPE html><html><head><title>SideGate</title></head><body><p>Upload successful!</p></body></html>`
+const TEMPLATE_INDEX string = `<!DOCTYPE html>
+<html>
+	<head>
+		<title>SideGate</title>
+		<style>
+			body {
+				line-height: 1.5;
+				font-family: "Helvetica", "Arial", sans-serif;
+				font-weight: 400;
+				font-size: 12pt;
+				color: #202020;
+
+				padding: 0.5em 1em;
+			}
+			h1 { font-size: 14pt; }
+			th { font-weight: 700; text-align: left; }
+			td { padding-left: 5px; padding-right: 5px; font-family: monospace; }
+			a:link    { color: #f99157; }
+			a:visited { color: #f99157; }
+			a:hover   { color: #faa61a; text-decoration: none; }
+		</style>
+	</head>
+	<body>
+		<h1>Upload a File</h1>
+		<form action="/upload" method="POST" enctype="multipart/form-data">
+			<div><input type="file" name="file"></div>
+			<div><input type="submit" value="Upload"></div>
+		</form>
+
+		<h1>{{.ServePath}}</h1>
+		<table>
+			{{range $item := .Items}}
+			<tr>
+				<td>{{$item.Size}}</td>
+
+				<td>
+				{{if $item.IsDir}}
+				{{$item.Name}}/
+				{{else}}
+				{{$item.Name}}
+				{{end}}
+				</td>
+			</tr>
+			{{end}}
+		</table>
+	</body>
+</html>`
+
+const TEMPLATE_UPLOAD string = `<!DOCTYPE html>
+<html>
+	<head>
+		<title>SideGate</title>
+		<style>
+			body {
+				line-height: 1.5;
+				font-family: "Helvetica", "Arial", sans-serif;
+				font-weight: 400;
+				font-size: 12pt;
+				color: #202020;
+
+				padding: 0.5em 1em;
+			}
+			h1 { font-size: 14pt; }
+			th { font-weight: 700; text-align: left; }
+			td { padding-left: 5px; padding-right: 5px; font-family: monospace; }
+			a:link    { color: #f99157; }
+			a:visited { color: #f99157; }
+			a:hover   { color: #faa61a; text-decoration: none; }
+		</style>
+	</head>
+
+	<body>
+		<p>Upload successful!</p>
+		<p><a href="/">Go back</a></p>
+	</body>
+</html>`
 
 const DEFAULT_LISTEN_PORT int = 8000
 
@@ -58,9 +134,62 @@ func uploadHandler(w http.ResponseWriter, r *http.Request, destinationDir string
 	t.Execute(w, nil)
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+type Node struct {
+	Name  string
+	Size  string
+	IsDir bool
+}
+
+type HomeDir struct {
+	ServePath string
+	Items     []Node
+}
+
+var suffixes = []string{"bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"}
+
+// This was borrowed from https://stackoverflow.com/a/25613067
+func humanizeFileSize(size int64) string {
+	if size == 1 {
+		return "1 byte"
+	}
+
+	order := uint(math.Log2(float64(size)) / 10.0)
+	denom := 1 << (order * 10)
+	realSize := float32(size) / float32(denom)
+	return fmt.Sprintf("%0.1f %s", realSize, suffixes[order])
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request, destDir string) {
 	t, _ := template.New("index-page").Parse(TEMPLATE_INDEX)
-	t.Execute(w, nil)
+
+	dirObjects, err := ioutil.ReadDir(destDir)
+	if err != nil {
+		log.Printf("Unable to read contents of directory %s: %v", destDir, err)
+		t.Execute(w, nil)
+		return
+	}
+
+	numObjects := len(dirObjects)
+	dirContents := make([]Node, numObjects)
+	for _, obj := range dirObjects {
+		var fileSize string
+		if obj.IsDir() {
+			fileSize = ""
+		} else {
+			fileSize = humanizeFileSize(obj.Size())
+		}
+
+		dirContents = append(dirContents, Node{
+			Name:  obj.Name(),
+			Size:  fileSize,
+			IsDir: obj.IsDir(),
+		})
+	}
+
+	t.Execute(w, HomeDir{
+		ServePath: destDir,
+		Items:     dirContents,
+	})
 }
 
 func main() {
@@ -79,7 +208,9 @@ func main() {
 	listenAddrStr.WriteString(strconv.Itoa(*listenPort))
 	listenAddress := listenAddrStr.String()
 
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		indexHandler(w, r, *destinationDir)
+	})
 	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		uploadHandler(w, r, *destinationDir)
 	})
