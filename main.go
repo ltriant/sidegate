b02d8972ba19f255a4ddbac7ac3a3d37ea946c4b
Luke Triantafyllidis <ltriant@cpan.org>
2020-02-27 15:05:48 +1100

    main: redirect to index page on upload
    Rather than having a useless page to say "all good", just redirect to
    the index page, which will show the file in the directory listing.
    
    I'd like to have some kind of "upload successful" message that I can put
    at the top of the index page, for informational purposes, but removing
    the upload page was a good first step.
    
    Also made the error conditions return a meaningful HTTP status code,
    instead of just letting log.Fatal panic and severing the client's
    connection.

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 	"strings"
 )
 
-// These are embedded because otherwise the binary needs to find template HTML
+// This is embedded because otherwise the binary needs to find template HTML
 // files at runtime, which I don't wanna mess around with for now. And other
 // solutions require third-party dependencies, which I'm trying to avoid unless
 // absolutely necessary.
@@ -32,12 +32,10 @@ const TEMPLATE_INDEX string = `<!DOCTYPE html>
 
 				padding: 0.5em 1em;
 			}
-			h1 { font-size: 14pt; }
+			h1 { font-size: 14pt; font-weight: 700; }
+			h2 { font-size: 12pt; font-weight: 700; }
 			th { font-weight: 700; text-align: left; }
 			td { padding-left: 5px; padding-right: 5px; font-family: monospace; }
-			a:link    { color: #f99157; }
-			a:visited { color: #f99157; }
-			a:hover   { color: #faa61a; text-decoration: none; }
 		</style>
 	</head>
 	<body>
@@ -47,7 +45,7 @@ const TEMPLATE_INDEX string = `<!DOCTYPE html>
 			<div><input type="submit" value="Upload"></div>
 		</form>
 
-		<h1>{{.ServePath}}</h1>
+		<h2>{{.ServePath}}</h2>
 		<table>
 			{{range $item := .Items}}
 			<tr>
@@ -66,35 +64,6 @@ const TEMPLATE_INDEX string = `<!DOCTYPE html>
 	</body>
 </html>`
 
-const TEMPLATE_UPLOAD string = `<!DOCTYPE html>
-<html>
-	<head>
-		<title>SideGate</title>
-		<style>
-			body {
-				line-height: 1.5;
-				font-family: "Helvetica", "Arial", sans-serif;
-				font-weight: 400;
-				font-size: 12pt;
-				color: #202020;
-
-				padding: 0.5em 1em;
-			}
-			h1 { font-size: 14pt; }
-			th { font-weight: 700; text-align: left; }
-			td { padding-left: 5px; padding-right: 5px; font-family: monospace; }
-			a:link    { color: #f99157; }
-			a:visited { color: #f99157; }
-			a:hover   { color: #faa61a; text-decoration: none; }
-		</style>
-	</head>
-
-	<body>
-		<p>Upload successful!</p>
-		<p><a href="/">Go back</a></p>
-	</body>
-</html>`
-
 const DEFAULT_LISTEN_PORT int = 8000
 
 func uploadHandler(w http.ResponseWriter, r *http.Request, destinationDir string) {
@@ -102,7 +71,9 @@ func uploadHandler(w http.ResponseWriter, r *http.Request, destinationDir string
 
 	fin, header, err := r.FormFile("file")
 	if err != nil {
-		log.Fatalf("Unable to get `file` parameter: %v", err)
+		log.Printf("Unable to get `file` parameter: %v", err)
+		http.Error(w, "Unable to get file data from request", http.StatusInternalServerError)
+		return
 	}
 	defer fin.Close()
 
@@ -118,20 +89,23 @@ func uploadHandler(w http.ResponseWriter, r *http.Request, destinationDir string
 
 	fout, err := os.Create(filePath)
 	if err != nil {
-		log.Fatalf("Unable to create file %s: %v", filePath, err)
+		log.Printf("Unable to create file %s: %v", filePath, err)
+		http.Error(w, "Unable to create file on disk", http.StatusInternalServerError)
+		return
 	}
 	defer fout.Close()
 
 	// Stream file to disk
 	bytes, err := io.Copy(fout, fin)
 	if err != nil {
-		log.Fatalf("Failed to save file to path %s: %v", filePath, err)
+		log.Printf("Failed to save file to path %s: %v", filePath, err)
+		http.Error(w, "Unable to save file", http.StatusInternalServerError)
+		return
 	}
 
 	log.Printf("File uploaded to: %s (%d bytes)", filePath, bytes)
 
-	t, _ := template.New("upload-page").Parse(TEMPLATE_UPLOAD)
-	t.Execute(w, nil)
+	http.Redirect(w, r, "/", http.StatusFound)
 }
 
 type Node struct {
@@ -159,9 +133,7 @@ func humanizeFileSize(size int64) string {
 	return fmt.Sprintf("%0.1f %s", realSize, suffixes[order])
 }
 
-func indexHandler(w http.ResponseWriter, r *http.Request, destDir string) {
-	t, _ := template.New("index-page").Parse(TEMPLATE_INDEX)
-
+func indexHandler(w http.ResponseWriter, r *http.Request, destDir string, t *template.Template) {
 	dirObjects, err := ioutil.ReadDir(destDir)
 	if err != nil {
 		log.Printf("Unable to read contents of directory %s: %v", destDir, err)
@@ -208,9 +180,15 @@ func main() {
 	listenAddrStr.WriteString(strconv.Itoa(*listenPort))
 	listenAddress := listenAddrStr.String()
 
+	indexTemplate, err := template.New("index-page").Parse(TEMPLATE_INDEX)
+	if err != nil {
+		log.Fatalf("Unable to build index page template: %v", err)
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		indexHandler(w, r, *destinationDir)
+		indexHandler(w, r, *destinationDir, indexTemplate)
 	})
+
 	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		uploadHandler(w, r, *destinationDir)
 	})
